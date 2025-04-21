d8459240b8d3f5789ae1448c8f13f9bfc3215f2c
Heena Kumawat <heenakumawat23k@gmail.com>
2025-04-21 09:43:41 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// connect mongodb to nodejs
+
+const { MongoClient } = require('mongodb');
+const url = 'mongodb://127.0.0.1:27017'; // Replace with your MongoDB connection string
+
+const client = new MongoClient(url);
+const dbName = 'mydatabase'; // Replace with your database name
+async function main(){
+    await client.connect();
+    console.log('Connected to MongoDB');
+    const db = client.db(dbName);
+    const collection = db.collection('people'); // Replace with your collection name
+    const findResult = await collection.find().toArray();
+    console.log('Found documents =>', findResult);
+}
+
+main();
